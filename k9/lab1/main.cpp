f3567a6bf8a4f8b0fd47fac0190b68690852d52e
Serafim Honcharov <sgoncharov-ee25@lll.kpi.ua>
2020-09-04 17:43:06 +0300

    lab0 for Beton is done

---
+++ k9/lab1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/numeric/ublas/matrix.hpp>
+
+using namespace boost::numeric::ublas;
+
+template<typename T>
+
+
+
+matrix<T> &Interpoalate(matrix<T> &input) {
+
+
+
+}
+
+
+int main() {
+
+
+	return 0;
+}
