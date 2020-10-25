ac9cf0a9d8ec39c53f50bcd4b3c6ca169c221b46
koz9va <serafim597@gmail.com>
2020-10-25 18:36:58 +0200

    backup lab3

--- k9/lab3/main.cpp
+++ k9/lab3/main.cpp
@@ -1,25 +1,33 @@
 #include <cstdio>
 #include <cmath>
 #include <cfloat>
-double f1(double x, int *cnt) {
-	++(*cnt);
+
+int cnt;
+
+double f1(double x) {
+	++cnt;
 	return x*x-3;
 }
 
-double f2(double x, int *cnt) {
-	++(*cnt);
-	return atan(x);
+double f2(double x) {
+	++cnt;
+	return 3e-10*(exp(x/(1.7*26e-3)) - 1);
+}
+
+double f3(double x) {
+	++cnt;
+	return x*x*x - sqrt(19);
 }
 
 
-double Bisection(double f(double, int*), double a, double b, double eps, int *cnt) {
+
+double Bisection(double f(double), double a, double b, double eps) {
 	double aSign, xi;
-	aSign = f(a, cnt)/(fabs(f(a, cnt)));
-	*cnt = 0;
+	aSign = f(a)/(fabs(f(a)));
 
 	do {
 		xi = (a+b)/2;
-		if(aSign * f(xi, cnt) < 0) {
+		if(aSign * f(xi) < 0) {
 			b = xi;
 		}else {
 			a = xi;
@@ -29,40 +37,44 @@ double Bisection(double f(double, int*), double a, double b, double eps, int *cn
 	return xi;
 }
 
-double Newton(double f(double, int*), double x0, double xt, double eps, int *cnt) {
+double Newton(double f(double), double x0, double xt, double eps, FILE* file) {
 	double x1, fx, h;
+
+	if(!file) {
+		printf("Wrong file pointer :(\n");
+		exit(21);
+	}
 
 	h = sqrt(DBL_EPSILON) * fmax(fabs(x0), fabs(xt));
 
-	fx = f(x0, cnt);
+	fx = f(x0);
 
-	x1 = x0 - (fx * h)/(f(x0+h, cnt) - fx);
+	x1 = x0 - (fx * h)/(f(x0+h) - fx);
 
-	while(fabs((x1 - x0) / x0) > eps) {
+	do {
+		fprintf(file, "%e\n", x0);
 		x0 = x1;
+		h = sqrt(DBL_EPSILON) * fmax(fabs(x0), fabs(xt));
 
-		fx = f(x0, cnt);
+		fx = f(x0);
 
-		x1 = x0 - (fx * h)/(f(x0+h, cnt) - fx);
+		x1 = x0 - (fx * h)/(f(x0+h) - fx);
 
-		while(fabs(f(x1, cnt)) > fabs(f(x0, cnt))) {
-			x1 = (x1 + x0) / 2;
-		}
 
-	}
+	} while (fabs((x1 - x0) / x0) > eps);
+
 
 	return x1;
 }
 
-double Secant(double f(double, int*), double x0, double x1, double eps, int *cnt) {
+double Secant(double f(double), double x0, double x1, double eps) {
 	double fx0, fx1, fx2, x2;
-
+	fx0 = f(x0);
+	fx1 = f(x1);
 	while(true) {
-		fx0 = f(x0, cnt);
-		fx1 = f(x1, cnt);
 
 		x2 = (x0 - (((x1-x0)/(fx1 - fx0)) * fx0));
-		fx2 = f(x2, cnt);
+		fx2 = f(x2);
 		if(fx2 == 0)
 			return x2;
 		if(fabs(x0 - x2) <= eps)
@@ -73,13 +85,15 @@ double Secant(double f(double, int*), double x0, double x1, double eps, int *cnt
 		else
 			x1 = x2;
 
+		fx0 = fx1;
+		fx1 = fx2;
+
 	}
 
 }
 
 int main() {
 
-	int cnt;
 	double fx;
 	FILE* file;
 
@@ -89,16 +103,17 @@ int main() {
 		return 21;
 	}
 	cnt = 0;
-	fx = Bisection(f1, 1.0, 2.0, 1e-6, &cnt);
+	fx = Bisection(f1, 1.0, 2.0, 1e-6);
 	fprintf(file, "Bisection method:\nThe root is: %e\nIterations count: %d\n", fx, cnt);
 
 	cnt = 0;
-	fx = Newton(f1, 2.0, 2.0, 1e-6, &cnt);
+
+	fx = Newton(f3, 1.0, 3.0, 1e-6, file);
 
 	fprintf(file, "Newton method:\nThe root is: %e\nIterations count: %d\n", fx, cnt);
 
 	cnt = 0;
-	fx = Secant(f1, 1.0, 2.0, 1e-6, &cnt);
+	fx = Secant(f3, -1.0, 3.0, 1e-6);
 
 	fprintf(file, "Secant method:\nThe root is: %e\nIterations count: %d\n", fx, cnt);
 
