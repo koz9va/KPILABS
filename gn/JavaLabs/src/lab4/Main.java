4e12395eeceffa08a1287b8974b80555d0c71583
gnida <dkuchin-ee25@gmail.com>
2020-11-09 15:41:29 +0200

    lab123151511

--- gn/JavaLabs/src/lab4/Main.java
+++ gn/JavaLabs/src/lab4/Main.java
@@ -1,15 +1,33 @@
 package lab4;
 
-import javax.imageio.ImageTranscoder;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.function.Function;
 
 public class Main {
+
+    private static int count = 0;
+
     public static void main(String[] args) {
         try(FileWriter writer = new FileWriter("Output.txt")) {
+            double eps = Double.parseDouble(args[0]);
+            double a = Double.parseDouble(args[1]);
+            double b = Double.parseDouble(args[2]);
+            double x0n = Double.parseDouble(args[3]);
+            double xt = Double.parseDouble(args[4]);
+            double x0s = Double.parseDouble(args[5]);
+            double x1 = Double.parseDouble(args[6]);
+
             Function<Double, Double> func = Main::function;
 
+            writer.write("Bisection\n");
+            writer.write("Solution: " + Bis(func, a, b, eps, writer) + " Count is: " + count + "\n");
+            count = 0;
+            writer.write("Newton\n");
+            writer.write("Solution: " + Newton(func, x0n, xt, eps, writer) + " Count is: " + count +"\n");
+            count = 0;
+            writer.write("Secant\n");
+            writer.write("Solution: " + Secant(func, x0s, x1, eps, writer) + " Count is: " + count +"\n");
         }
         catch (IOException ignored){}
     }
@@ -48,7 +66,7 @@ public class Main {
             x1 = x0 - (fx * h) / (func.apply(x0 + h) - fx);
             writer.write(x1 + "\n");
         }
-        while (Math.abs((x1 - x0) / x0) > eps);
+        while (Math.abs((x1 - x0) / x0) > eps * Math.abs(x1));
         return x1;
     }
 
@@ -57,28 +75,29 @@ public class Main {
                                  double x1,
                                  double eps,
                                  FileWriter writer) throws IOException {
-        double fx0, fx1, fx2, x2;
-        fx0 = func.apply(x0);
+        double fx0, fx1, fx2, x2, h;
+        h = Math.sqrt(Math.ulp(1.0)) * x0;
         fx1 = func.apply(x1);
-        while (true) {
-            x2 = (x0 - (((x1 - x0) / (fx1 - fx0)) * fx0));
+        x2 = x1 - (h * fx1) / (func.apply(x0 + h) - fx1);
+        fx2 = func.apply(x2);
+        do {
             writer.write(x2 + "\n");
-            fx2 = func.apply(x2);
-            if (fx2 == 0)
-                return x2;
-            if (Math.abs(x0 - x2) <= eps)
-                return x2;
-            if ((fx0) / Math.abs(fx0) == fx2 / Math.abs(fx2))
-                x0 = x2;
-            else
-                x1 = x2;
+            x0 = x1;
+            x1 = x2;
             fx0 = fx1;
             fx1 = fx2;
+            x2 = x1 - (((x1 - x0) * fx1) / (fx1 - fx0));
+            fx2 = func.apply(x2);
         }
+        while (Math.abs(x2 - x1) > eps * Math.abs(x1));
+        return x2;
     }
 
-    private static Double function(Double a){
-
+    private static Double function(Double Ube) {
+        count++;
+        double Ie, Eb = 4, Ec = 10, R = 5e3, Ie0 = 1e-9, Me = 1.01, Fit = 26e-3, h21 = 2e-3;
+        Ie = Ie0 * (Math.exp((Ube + h21 * (Ec - (Eb - Ube)))/(Me * Fit)) - 1);
+        return Ie * R;
     }
 
 }
