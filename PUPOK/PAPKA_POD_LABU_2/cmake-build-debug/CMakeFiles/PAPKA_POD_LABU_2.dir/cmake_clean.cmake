69e07019bd663948832d3b7f8eb46662b08447ba
RBRy666 <realbun0777@gmail.com>
2020-12-06 16:52:38 +0200

    lab5

---
+++ PUPOK/PAPKA_POD_LABU_2/cmake-build-debug/CMakeFiles/PAPKA_POD_LABU_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAPKA_POD_LABU_2.dir/main.cpp.o"
+  "PAPKA_POD_LABU_2.exe"
+  "PAPKA_POD_LABU_2.pdb"
+  "libPAPKA_POD_LABU_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PAPKA_POD_LABU_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
