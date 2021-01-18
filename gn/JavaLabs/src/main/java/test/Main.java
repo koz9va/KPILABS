a2d64cbb9af23338ef83e6e5856f49a3243ce662
gnida <dkuchin-ee25@gmail.com>
2021-01-18 15:28:27 +0200

    test matlab

--- gn/JavaLabs/src/main/java/test/Main.java
+++ gn/JavaLabs/src/main/java/test/Main.java
@@ -5,23 +5,14 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        double x = -0.72;
-        double x2 = -0.54;
+        int x = Integer.MIN_VALUE;
 
-        double a = 2 * x + x2 / 2 + 2 + 4 * Math.pow(x + x2 + 1, 3);
-        double b = x2 / 8 + x / 2 + 0.5 + 4 * Math.pow(x + x2 + 1, 3);
-
-        System.out.println(a);
-        System.out.println(b);
-
-        System.out.println("------------------------------");
-
-        System.out.print(12 * (x + x2 + 1) * (x + x2 + 1) + 2);
-        System.out.print(" ");
-        System.out.println(12 * (x + x2 + 1) * (x + x2 + 1) + 0.5);
-        System.out.print(12 * (x + x2 + 1) * (x + x2 + 1) + 0.5);
-        System.out.print(" ");
-        System.out.println(12 * (x + x2 + 1) * (x + x2 + 1) + 1.0/8);
+        System.out.println(x);
+        int a = -1;
+        int y = x * a;
+        int z = x;
+        System.out.println(y);
+        System.out.println(z);
     }
 
 }
