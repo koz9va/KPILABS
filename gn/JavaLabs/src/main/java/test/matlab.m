b5fc002cbb17c85e0cf645b92de3591f7c9223a4
dmitrij <livekongo@gmail.com>
2021-02-08 17:43:05 +0200

    semi-ready lab1 MMF

--- gn/JavaLabs/src/main/java/test/matlab.m
+++ gn/JavaLabs/src/main/java/test/matlab.m
@@ -94,12 +94,37 @@ end
 
 
 
-%S = svd(inputMat);
-%
-%Solve = inputB ./ S
-%
-%printf("CondInputmat\n");
-%Cond = cond(inputMat)
+[U, S, V] = svd(inputMat);
+
+C = U' * inputB;
+
+
+max = S(1,1);
+min = S(1,1);
+for i = 1:size(S,1)
+    if S(i,i) > max
+        max = S(i,i);
+    end
+    if S(i,i) < min
+        min = S(i,i)
+    end
+end
+
+t = eps * max;
+Y = zeros(size(S, 1), 1);
+for i = 1:size(S,1)
+    if S(i,i) > t
+        Y(i) = C(i) / S(i,i);
+    else
+        Y(i) = 0;
+    end
+end
+
+printf("SVD_fact\n")
+SolveVecSvd = V * Y
+
+printf("CondA\n");
+Cond = max / min
 
 
 
