0b9efd80af69ad13ae04c26f112b837015c21af2
koz9va <serafim597@gmail.com>
2021-02-23 12:26:26 +0200

    abs tau fixed in svd

--- k9/matlab/lab1/SVD_roots.m
+++ k9/matlab/lab1/SVD_roots.m
@@ -2,11 +2,11 @@ function roots_svd = SVD_roots(A, B)
 	[U, S, V] = svd(A);
 	C = U' * B;
 
-	tau = eps * max(diag(S));
+	tau = eps * abs(max(diag(S)));
 	Y = zeros(size(B));
 
 	for i = 1:size(B, 1)
-		if(S(i, i) > tau)
+		if(abs(S(i, i)) > tau)
 			Y(i) = C(i) / S(i, i);
 		else
 			Y(i) = 0;
