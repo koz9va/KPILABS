d4f9c160b09a92acc85e2dfbf3f88ca4de70ba28
dmytro <dkuchin-ee25@lll.kpi.ua>
2021-08-28 23:34:06 +0300

    start doing first python lab

--- JavaLabs/src/main/java/sem5/lab1/main.py
+++ JavaLabs/src/main/java/sem5/lab1/main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# point 4
+def funcLinearOneDem(value, scale, offsetX, offsetY):
+    return (scale * value + offsetX) + offsetY
+
+
+x = np.linspace(-5, 5)
+y = funcLinearOneDem(x, 1, 2, 0)
+plt.plot(x, y)
+plt.grid()
+plt.title("Linear Function")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
